548d8897161343cf5e3f2895cee82baba9498f47
Nikolay Anikeev <n.anikeev@gmail.com>
2019-08-13 00:23:54 +0300

    Добавлен Swagger в APi

--- CarAssistance/Startup.cs
+++ CarAssistance/Startup.cs
@@ -8,8 +8,15 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace CarAssistance
 {
+    /// <summary>
+    /// Point start up
+    /// </summary>
     public class Startup
     {
+        /// <summary>
+        /// Constructor 
+        /// </summary>
+        /// <param name="configuration">configuration for start up</param>
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -27,10 +34,29 @@ namespace CarAssistance
             var mappingConfig = new MapperConfiguration(conf => { conf.AddProfile(new MappingProfile()); });
             IMapper mapper = mappingConfig.CreateMapper();
             services.AddSingleton(mapper);
+            services.AddSwaggerDocument(conf =>
+            {
+                conf.PostProcess = document =>
+                {
+                    document.Info.Version = "v1";
+                    document.Info.Title = "CarAssistance API";
+                    document.Info.Contact = new NSwag.OpenApiContact
+                    {
+                        Name = "Nikolay Anikeev",
+                        Email = "n.anikeev@gmail.com",
+                        Url = "https://github.com/zestt132hp"
+                    };
+                };
+            });
+
             services.AddMvc();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// </summary>
+        /// <param name="app">application builder</param>
+        /// <param name="env">host environment</param>
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -42,6 +68,8 @@ namespace CarAssistance
                 app.UseHsts();
             }
 
+            app.UseOpenApi();
+            app.UseSwaggerUi3();
             app.UseHttpsRedirection();
             app.UseMvc();
         }
